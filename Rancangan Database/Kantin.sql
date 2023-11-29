5edbb8104c70c1834f8f61c0b0ff8f9c64ba6e78
RafliArafat <rafliarafat12@gmail.com>
2023-11-29 10:02:25 +0700

    perubahan database

--- Rancangan Database/Kantin.sql
+++ Rancangan Database/Kantin.sql
@@ -2,12 +2,12 @@
 /* DBMS name:      MySQL 5.0                                    */
 /* Created on:     28/11/2023 08:58:55                          */
 /*==============================================================*/
-
+DROP DATABASE if EXISTS Kantin;
 CREATE DATABASE Kantin;
 use Kantin;
 drop table if exists DETAILTRANSAKSISUPPLIER;
 
-drop table if exists DETAI_TRANSAKSI;
+drop table if exists DETAIL_TRANSAKSI;
 
 drop table if exists PRODUK;
 
@@ -28,24 +28,24 @@ create table DETAILTRANSAKSISUPPLIER
    JUMLAH               int,
    TANGGALSUPPLY        date,
    HARGASUPPLIER        decimal(10,3),
-   "RETURN"             decimal(10,3),
+   RETUR               decimal(10,3),
    primary key (ID_DETAILTRANSAKSI)
 );
 
 /*==============================================================*/
 /* Table: DETAI_TRANSAKSI                                       */
 /*==============================================================*/
-create table DETAI_TRANSAKSI
+create table DETAIL_TRANSAKSI
 (
-   ID_DETAI_TRANSAKSI   int not null,
-   ID_PRODUK            int not null,
-   ID_TRANSAKSI         int not null,
-   QTY                  int not null,
-   HARGA                decimal(10,3) not null,
-   SUB_TOTAL            decimal(10,3) not null,
-   JUMLAH_BAYAR         decimal(10,3) not null,
-   SISA_BAYAR           decimal(10,3) not null,
-   primary key (ID_DETAI_TRANSAKSI)
+   ID_DETAIL_TRANSAKSI   int not null,
+   ID_PRODUK            int,
+   ID_TRANSAKSI         int,
+   QTY                  int,
+   HARGA                decimal(10,3),
+   SUB_TOTAL            decimal(10,3),
+   JUMLAH_BAYAR         decimal(10,3),
+   SISA_BAYAR           decimal(10,3),
+   primary key (ID_DETAIL_TRANSAKSI)
 );
 
 /*==============================================================*/
@@ -54,10 +54,10 @@ create table DETAI_TRANSAKSI
 create table PRODUK
 (
    ID_PRODUK            int not null,
-   NAMA_PRODUK          varchar(100) not null,
-   KATEGORI             varchar(100) not null,
-   STOK                 int not null,
-   HARGA                decimal(10,3) not null,
+   NAMA_PRODUK          varchar(100),
+   KATEGORI             varchar(100),
+   STOK                 int,
+   HARGA                decimal(10,3),
    primary key (ID_PRODUK)
 );
 
@@ -68,7 +68,7 @@ create table SUPPLIER
 (
    SUPPLIER_ID          int not null,
    NAMA_SUPPLIER        varchar(100) not null,
-   NO_TELP              varchar(15) not null,
+   NO_TELP              varchar(15),
    primary key (SUPPLIER_ID)
 );
 
@@ -79,9 +79,9 @@ create table TRANSAKSI
 (
    ID_TRANSAKSI         int not null,
    ID_USER              int not null,
-   JENIS_PEMBAYARAN     varchar(50) not null,
-   TGL_TRANSAKSI        datetime not null,
-   TOTAL_HARGA          decimal(10,3) not null,
+   JENIS_PEMBAYARAN     varchar(50),
+   TGL_TRANSAKSI        datetime,
+   TOTAL_HARGA          decimal(10,3),
    primary key (ID_TRANSAKSI)
 );
 
@@ -91,10 +91,10 @@ create table TRANSAKSI
 create table USERS
 (
    ID_USER              int not null,
-   NAMA                 varchar(100) not null,
-   USER_NAME            varchar(100) not null,
-   PASSWORD             varchar(100) not null,
-   LEVEL                varchar(50) not null,
+   NAMA                 varchar(100),
+   USER_NAME            varchar(100),
+   PASSWORD             varchar(100),
+   LEVEL                enum("Pemilik","Kasir"), 
    primary key (ID_USER)
 );
 
@@ -104,10 +104,10 @@ alter table DETAILTRANSAKSISUPPLIER add constraint FK_MENSUPLY foreign key (ID_P
 alter table DETAILTRANSAKSISUPPLIER add constraint FK_MENSUPLY2 foreign key (SUPPLIER_ID)
       references SUPPLIER (SUPPLIER_ID);
 
-alter table DETAI_TRANSAKSI add constraint FK_MELIBATKAN foreign key (ID_PRODUK)
+alter table DETAIL_TRANSAKSI add constraint FK_MELIBATKAN foreign key (ID_PRODUK)
       references PRODUK (ID_PRODUK);
 
-alter table DETAI_TRANSAKSI add constraint FK_MELIBATKAN2 foreign key (ID_TRANSAKSI)
+alter table DETAIL_TRANSAKSI add constraint FK_MELIBATKAN2 foreign key (ID_TRANSAKSI)
       references TRANSAKSI (ID_TRANSAKSI);
 
 alter table TRANSAKSI add constraint FK_MELAYANI foreign key (ID_USER)

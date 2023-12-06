ac8b91ee0e92abe17a7b71256ba9ea5f2ac09f1a
JoyoS58 <joyosugit58@gmail.com>
2023-12-06 08:45:26 +0700

    login.php

--- Tugas_Akhir_pakaiIniSaja/Login.php
+++ Tugas_Akhir_pakaiIniSaja/Login.php
@@ -13,11 +13,11 @@ if (session_status() === PHP_SESSION_NONE) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kantin JTI</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-  <link rel="stylesheet" href="Login.css">
+  <link rel="stylesheet" href="components/css/Login.css">
 </head>
 <body>
   <div class="header">
-    <img src="img/Logo-JTI.png" alt="">
+    <img src="assets/Logo-JTI.png" alt="">
     <h1>Kantin JTI POLINEMA</h1>
   </div>
   <div class="login-box">

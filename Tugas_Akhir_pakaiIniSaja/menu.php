aff2337555e95b952216880bf2c0723353862441
JoyoS58 <joyosugit58@gmail.com>
2023-12-06 09:05:47 +0700

    koneksi css

--- Tugas_Akhir_pakaiIniSaja/menu.php
+++ Tugas_Akhir_pakaiIniSaja/menu.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <title>Sidebar Menu | Side Navigation Bar</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="components/css/styleSidebar.css">
+    <link rel="stylesheet" href="components/css/styleSidebarKasir.css">
     <!-- Boxicons CSS -->
     <link
       href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"

80ec9abe8a4194524c1a350566d8df91a420010b
RafliArafat <rafliarafat12@gmail.com>
2023-12-06 10:48:38 +0700

    perubahan index untuk percobaan kasir

--- Tugas_Akhir_pakaiIniSaja/index.php
+++ Tugas_Akhir_pakaiIniSaja/index.php
@@ -1,26 +1,27 @@
-x<?php
+<?php
 if (session_status()=== PHP_SESSION_NONE){
     session_start();
 }
-if(empty($_SESSION['level'])){
-    require 'config/koneksi.php';
-    require 'function/pesanKilat.php';
-    if($_SESSION['level']=="kasir"){
-        include 'head.php';
+// $user=$_GET['username'];
+// if(!empty($user)){
+    // require 'config/koneksi.php';
+    // require 'function/pesanKilat.php';
+    // if($user=="kasir"){
         if(!empty($_GET['page'])){
-            include 'user/' . $_GET['page'] . '/index.php';
+            // include 'admin/module/' . $_GET['page'] . '/index.php';
         } else{
             include 'home.php';
         }
+        include 'menu.php';
         include 'Footer.php';
-    } else{
-        include 'headPemilik.php';
-        if(!empty($_GET['page'])){
-            include 'user' . $_GET['page'] . '/index.php';
-        } else{
-            include 'homePemilik.php';
-        }
-    }
-} else{
-    header("Location: Login.php");
-}
+    // } else{
+        // include 'admin/template/headPemilik.php';
+        // if(!empty($_GET['page'])){
+        //     // include 'admin/module/' . $_GET['page'] . '/index.php';
+        // } else{
+        //     // include 'admin/template/homePemilik.php';
+        // }
+    // }
+// } else{
+//     header("Location: Login.php");
+// } 

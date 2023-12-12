2147a266f7ef005ee8b114886474080ea0645b99
JoyoS58 <joyosugit58@gmail.com>
2023-12-12 09:33:43 +0700

    update cek login

--- Tugas_Akhir_pakaiIniSaja/cek_login.php
+++ Tugas_Akhir_pakaiIniSaja/cek_login.php
@@ -1,15 +1,15 @@
 <?php
-if(session_status()===PHP_SESSION_NONE){
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 include "config/koneksi.php";
-    include "function/pesanKilat.php";
-    include "function/antiInjection.php";
+include "function/pesanKilat.php";
+include "function/antiInjection.php";
 
-    $username= antiinjection($koneksi, $_POST['username']);
-    $password= antiinjection($koneksi, $_POST['password']);
+$username = antiinjection($koneksi, $_POST['username']);
+$password = antiinjection($koneksi, $_POST['password']);
 
-    $query = "SELECT username, level, password FROM user WHERE username = '$username'";
+$query = "SELECT username, level, password FROM users WHERE username = '$username'";
 $result = mysqli_query($koneksi, $query);
 
 if ($result) {
@@ -21,17 +21,17 @@ if ($result) {
         $hashed_password = password_hash($row['password'], PASSWORD_DEFAULT);
         echo $password . "password <br>";
         echo $hashed_password . "hashed <br>";
-        if($hashed_password!==null){
-            if(password_verify($password, $hashed_password)){
+        if ($hashed_password !== null) {
+            if (password_verify($password, $hashed_password)) {
                 $_SESSION['username'] = $row['username'];
                 $_SESSION['level'] = $row['level'];
                 header("Location: index.php");
-            } else{
+            } else {
                 pesan('danger', "Login gagal. Password Anda Salah");
                 header("Location: login.php");
                 // echo "Apa ini";
             }
-        } else{
+        } else {
             pesan('warning', "Username tidak ditemukan.");
             header("Location: login.php");
             // echo "Atau ini";
